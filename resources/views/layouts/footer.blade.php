4feb87f3dd680e98cb973de6999a67c7ca8f942e
Eliagtz <eliacer.guitierrez@alumnos.udg.mx>
2019-03-27 21:34:43 -0600

    Footer created

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="app-footer">
+    <div>
+        <a href="https://www.facebook.com/eliia.gutiierrez" target="_blank">
+            <i class="fab fa-facebook-square fa-2x"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/eliacer-gutierrez-solis-a9a514179/" target="_blank">
+            <i class="fab fa-linkedin fa-2x"></i>
+        </a>
+        <a href="https://twitter.com/eliiagutiierrez" target="_blank">
+            <i class="fab fa-twitter-square fa-2x"></i>
+        </a>
+    </div>
+    <div class="ml-auto">
+            <span>© 2018 Elia Gutierrez Web Developer.</span>
+    </div>
+</footer>
